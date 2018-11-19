e9d8e1cdb0b85d7563f2904d8c33967135540683
Elio Champenois <elio.champenois@gmail.com>
2018-11-19 15:30:49 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 		  packages=['cart2pol'],
 		  install_requires=['numpy', 'h5py', 'scipy', 'quadrant'],
 		  version='1.0.0',
-		  description='Image rebinning between cartesian to polar coordinates.',
+		  description='Image rebinning between cartesian and polar coordinates.',
 		  url='https://github.com/e-champenois/cart2pol',
 		  author='Elio Champenois',
 		  author_email='elio.champenois@gmail.com',
